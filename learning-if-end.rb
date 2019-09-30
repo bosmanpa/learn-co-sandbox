87b0ef1fa8c380ad54cbaf4411957bc66a52b70b
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-09-30 23:48:28 +0000

    Automatically backed up by Learn

---
+++ learning-if-end.rb
@@ -0,0 +1,21 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+#chance_of_rain = -23
+#puts "Let's go outside!"
+#if chance_of_rain <= 0.25
+# puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay at home and read Hegel."
+#end
+#puts "Oh, and always wear sunscreen!"
+
+puts "You know what year it is??"
+this_year = 
+puts "Hey, it's 2019!" if this_year == 2019
